e8676d25ccc306e1a92e665b911efedca2911176
SirGlory <29068174+SirGlory@users.noreply.github.com>
2021-04-09 00:54:27 +0200

    FIx file saving issues
    Would not render latest map render. Now will create unique maps for each @handle, and delete any old occurrences with same name.

--- main.py
+++ main.py
@@ -23,17 +23,20 @@ class HomePage(MethodView):
 class MapPage(MethodView):
 
     def post(self):
-        if os.path.exists("templates\mapped_locations.html"):
-            print("mapped_locations.html, file deleted!")
-            os.remove("templates\mapped_locations.html")
-        else:
-            print("mapped_locations.html does not exist")
-        
         handle_form = HandleForm(request.form)
         handle = str(handle_form.handle.data)
         locations = Scrape(handle).get_locations()
-        mymap = Map(location=[-20, 20], zoom_start=5)
+        my_map = Map(location=[-20, 20], zoom_start=5)
+        map_name = f"templates\map_{handle}.html"
+        map_name_return = f"map_{handle}.html"
 
+        # Delete any old occurrences with same name
+        if os.path.exists(map_name):
+            print(f"{map_name}, file deleted!")
+            os.remove(map_name)
+        else:
+            print(f"{map_name} does not exist")
+            
         for i in range(0, len(locations), 1):
             # Convert address to coordinates
             locator = Nominatim(user_agent="myGeocoder")
@@ -45,16 +48,17 @@ class MapPage(MethodView):
                 geopoint = Geopoint(latitude=lat, longitude=lon)
                 popup = Popup(locations[i], max_width=400)
                 popup.add_to(geopoint)
-                geopoint.add_to(mymap)
+                geopoint.add_to(my_map)
                 print(locationi.address)
             except:
                 pass
 
         # Save the Map Instance Into a HTML file
-        mymap.save("templates/mapped_locations.html")
+        my_map.save(map_name)
+        print(map_name_return)
         print("------------------------------------------")
-        print("Map Created! Check your files for mapped_locations.html")
-        return render_template('mapped_locations.html')
+        print(f"Map Created! Check your files for {map_name}")
+        return render_template(map_name_return, handle=handle, handleform=handle_form)
 
 
 class AboutPage(MethodView):
@@ -75,6 +79,7 @@ app.add_url_rule('/map_page',
                  view_func=MapPage.as_view('map_page'))
 app.add_url_rule('/about_page',
                  view_func=AboutPage.as_view('about_page'))
+
 if __name__ == '__main__':
     app.run()
 
