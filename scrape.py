1744557e684dda084defabe593412cbf1c9ef8c6
SirGlory <29068174+SirGlory@users.noreply.github.com>
2021-04-15 22:06:49 +0200

    Added Database -increase functionality, more stable

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 # Imports
 import requests
 from bs4 import BeautifulSoup
+from geopy import Nominatim
 
 
 class Scrape:
@@ -17,30 +18,58 @@ class Scrape:
                              'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
         c = r.content
         soup = BeautifulSoup(c, "html.parser")
-        all_locs = soup.find_all("div", {"class": "photo-location"})
+        all_locations = soup.find_all("div", {"class": "photo-location"})
+        # print(all_locations)
         all_links = soup.find_all("div", {"class": "photo"})
-        locs = []
+        locations = []
         links = []
-
+        latitudes = []
+        longitudes = []
         # Add unique locations to list
-        for i in range(0, len(all_locs), 1):
-            xx = all_locs[i].text.replace("\n", "")
+        for i in range(0, len(all_locations), 1):
+            locator = Nominatim(user_agent="myGeocoder")
+            xx = all_locations[i].text.replace("\n", "")
             yy = all_links[i].find('a').get('href')
-            if xx not in locs:
-                locs.append(xx)
-                links.append(yy)
-            else:
+            try:
+                locationi = locator.geocode(xx)
+                zz = locationi.latitude
+                ww = locationi.longitude
+                # print(zz)
+                if xx not in locations:
+                    if xx != '':
+                        locations.append(xx)
+                        links.append(yy)
+                        latitudes.append(zz)
+                        longitudes.append(ww)
+                    else:
+                        pass
+                else:
+                    pass
+            except:
                 pass
+        else:
+            pass
 
-        reversed_locs = locs[::-1]  # Revered location order. scraping puts newest first,
-        reversed_links = links[::-1] # but to plot journey we would want oldest post as starting point
-        return reversed_locs, reversed_links
+
+        #
+        reversed_locations = locations[::-1]
+        reversed_links = links[::-1]
+        reversed_latitudes = latitudes[::-1]
+        reversed_longitudes = longitudes[::-1]
+        # print(locs)
+        # print(links)
+        # print(base_url)
+        return reversed_locations, reversed_links, reversed_latitudes, reversed_longitudes  # Revered location order. scraping puts newest first,
+        # return l            # but to plot journey we would want oldest post as starting point
 
 
 if __name__ == "__main__":
-    handle = str(input("Please enter handle of user: ") or '4x4theboiz')
+    #handle = str(input("Please enter handle of user: ") or '4x4theboiz')
+    handle = "4x4theboiz"
     print(handle)
     point1 = Scrape(handle=handle)
-    address = point1.get_locations()
+    address = Scrape(handle=handle).get_locations()
     print(address[0])
     print(address[1])
+    print(address[2])
+    print(address[3])
